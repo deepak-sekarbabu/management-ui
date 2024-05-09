42648588f8429091fdff163aaff7ea339683c5b9
Deepak Sekarbabu <deepakinmail@gmail.com>
2024-05-09 22:24:47 +0200

    Fixes for Absence Select Box

--- src/sections/doctorabsence/doctor-absence-view.jsx
+++ src/sections/doctorabsence/doctor-absence-view.jsx
@@ -3,32 +3,57 @@ import React, { useState } from 'react';
 import Alert from '@mui/material/Alert';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { DatePicker, TimePicker, LocalizationProvider } from '@mui/x-date-pickers';
-import { Box, Card, Paper, Table, Button, MenuItem, TableRow, TableBody, TextField, TableCell, TableHead, Typography, TableContainer } from '@mui/material';
+import {
+    Select,
+    FormHelperText,
+    Box,
+    Card,
+    Paper,
+    Table,
+    Button,
+    MenuItem,
+    TableRow,
+    TableBody,
+    TextField,
+    TableCell,
+    TableHead,
+    Typography,
+    TableContainer,
+} from '@mui/material';
 
 import data from './data.json';
 
 const doctorData = [
     {
-        "id": 1,
-        "doctorId": "AB00001",
-        "doctorName": "Dr. Deepak Sekarbabu"
+        id: 1,
+        doctorId: 'AB00001',
+        doctorName: 'Dr. Deepak Sekarbabu',
     },
     {
-        "id": 2,
-        "doctorId": "AB00002",
-        "doctorName": "Dr. Dilip Sekarbabu"
-    }
+        id: 2,
+        doctorId: 'AB00002',
+        doctorName: 'Dr. Dilip Sekarbabu',
+    },
 ];
 
 const DoctorAbsencePage = () => {
     const [doctorAbsence, setDoctorAbsence] = useState([...data]);
-    const [newRow, setNewRow] = useState({ doctorId: '', doctorName: '', absenceDate: '', absenceStartTime: '', absenceEndTime: '', optionalMessage: '' });
+    const [newRow, setNewRow] = useState({
+        doctorId: '',
+        doctorName: '',
+        absenceDate: '',
+        absenceStartTime: '',
+        absenceEndTime: '',
+        optionalMessage: '',
+    });
     const [isAdding, setIsAdding] = useState(false);
     const [showErrorAlert, setShowErrorAlert] = useState(false);
 
     const handleRemove = (id) => {
-        console.log("Removing row with ID:", id)
-        setDoctorAbsence(prevDoctorAbsence => prevDoctorAbsence.filter(absence => absence.id !== id));
+        console.log('Removing row with ID:', id);
+        setDoctorAbsence((prevDoctorAbsence) =>
+            prevDoctorAbsence.filter((absence) => absence.id !== id)
+        );
     };
 
     const handleAdd = () => {
@@ -44,11 +69,13 @@ const DoctorAbsencePage = () => {
             return;
         }
 
-        const formattedAbsenceDate = newRow.absenceDate.toLocaleDateString('en-GB', {
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric',
-        }).replace(/\//g, '-');
+        const formattedAbsenceDate = newRow.absenceDate
+            .toLocaleDateString('en-GB', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+            })
+            .replace(/\//g, '-');
 
         const formattedAbsenceStartTime = newRow.absenceStartTime.toLocaleTimeString('en-US', {
             hour: '2-digit',
@@ -74,30 +101,54 @@ const DoctorAbsencePage = () => {
             optionalMessage: newRow.optionalMessage,
         };
 
-        console.log("New Row:", newAbsence);
-        console.log("Adding new row:", newAbsence);
+        console.log('New Row:', newAbsence);
+        console.log('Adding new row:', newAbsence);
         setDoctorAbsence([...doctorAbsence, newAbsence]);
-        setNewRow({ doctorId: '', doctorName: '', absenceDate: '', absenceStartTime: '', absenceEndTime: '', optionalMessage: '' });
+        setNewRow({
+            doctorId: '',
+            doctorName: '',
+            absenceDate: '',
+            absenceStartTime: '',
+            absenceEndTime: '',
+            optionalMessage: '',
+        });
         setIsAdding(false);
     };
 
     const handleCancel = () => {
-        console.log("Canceling addition of new row:", newRow);
-        setNewRow({ doctorId: '', doctorName: '', absenceDate: '', absenceStartTime: '', absenceEndTime: '', optionalMessage: '' });
+        console.log('Canceling addition of new row:', newRow);
+        setNewRow({
+            doctorId: '',
+            doctorName: '',
+            absenceDate: '',
+            absenceStartTime: '',
+            absenceEndTime: '',
+            optionalMessage: '',
+        });
         setIsAdding(false);
     };
 
     const handleDoctorIdChange = (event) => {
         const selectedDoctorId = event.target.value;
-        const selectedDoctor = doctorData.find(doctor => doctor.doctorId === selectedDoctorId);
-        setNewRow({ ...newRow, doctorId: selectedDoctorId, doctorName: selectedDoctor ? selectedDoctor.doctorName : '' });
+        const selectedDoctor = doctorData.find((doctor) => doctor.doctorId === selectedDoctorId);
+        setNewRow({
+            ...newRow,
+            doctorId: selectedDoctorId,
+            doctorName: selectedDoctor ? selectedDoctor.doctorName : '',
+        });
     };
 
     return (
         <Card>
-            {showErrorAlert && <Alert severity="error">Please fix the validation errors in the page before saving</Alert>}
+            {showErrorAlert && (
+                <Alert severity="error">
+                    Please fix the validation errors in the page before saving
+                </Alert>
+            )}
             <Box p={2}>
-                <Typography style={{ marginBottom: '20px' }} variant="h2">Doctor Absence Information</Typography>
+                <Typography style={{ marginBottom: '20px' }} variant="h2">
+                    Doctor Absence Information
+                </Typography>
                 <TableContainer component={Paper}>
                     <Table aria-label="simple table">
                         <TableHead>
@@ -121,38 +172,55 @@ const DoctorAbsencePage = () => {
                                     <TableCell>{absence.absenceEndTime}</TableCell>
                                     <TableCell>{absence.optionalMessage}</TableCell>
                                     <TableCell>
-                                        <Button variant="outlined" color="error" onClick={() => handleRemove(absence.id)}>Remove</Button>
+                                        <Button
+                                            variant="outlined"
+                                            color="error"
+                                            onClick={() => handleRemove(absence.id)}
+                                        >
+                                            Remove
+                                        </Button>
                                     </TableCell>
                                 </TableRow>
                             ))}
                             {isAdding && (
                                 <TableRow>
                                     <TableCell>
-                                        <TextField
-                                            select
+                                        <Select
                                             value={newRow.doctorId}
-                                            onChange={handleDoctorIdChange}
+                                            onChange={(e) => handleDoctorIdChange(e)}
                                             error={newRow.doctorId === ''}
-                                            helperText={newRow.doctorId === '' ? 'Doctor ID is required' : ''}
+                                            displayEmpty
+                                            renderValue={(value) =>
+                                                value === '' ? 'Select Doctor ID' : value
+                                            }
                                         >
-                                            <MenuItem value="">Select Doctor ID</MenuItem>
                                             {doctorData.map((doctor) => (
-                                                <MenuItem key={doctor.doctorId} value={doctor.doctorId}>{doctor.doctorId}</MenuItem>
+                                                <MenuItem
+                                                    key={doctor.doctorId}
+                                                    value={doctor.doctorId}
+                                                >
+                                                    {doctor.doctorId}
+                                                </MenuItem>
                                             ))}
-                                        </TextField>
+                                        </Select>
+                                        {newRow.doctorId === '' && (
+                                            <FormHelperText error>
+                                                Doctor ID is required
+                                            </FormHelperText>
+                                        )}
                                     </TableCell>
+
                                     <TableCell>
-                                        <TextField
-                                            value={newRow.doctorName}
-                                            disabled
-                                        />
+                                        <TextField value={newRow.doctorName} disabled />
                                     </TableCell>
                                     <TableCell>
                                         <LocalizationProvider dateAdapter={AdapterDateFns}>
                                             <DatePicker
                                                 label="Absence Date"
                                                 value={newRow.absenceDate}
-                                                onChange={(date) => setNewRow({ ...newRow, absenceDate: date })}
+                                                onChange={(date) =>
+                                                    setNewRow({ ...newRow, absenceDate: date })
+                                                }
                                                 format="dd/MM/yyyy"
                                                 inputFormat="dd/MM/yyyy"
                                                 mask="__/__/____"
@@ -165,7 +233,12 @@ const DoctorAbsencePage = () => {
                                             <TimePicker
                                                 label="Absence Start Time"
                                                 value={newRow.absenceStartTime}
-                                                onChange={(newValue) => setNewRow({ ...newRow, absenceStartTime: newValue })}
+                                                onChange={(newValue) =>
+                                                    setNewRow({
+                                                        ...newRow,
+                                                        absenceStartTime: newValue,
+                                                    })
+                                                }
                                             />
                                         </LocalizationProvider>
                                     </TableCell>
@@ -174,12 +247,20 @@ const DoctorAbsencePage = () => {
                                             <TimePicker
                                                 label="Absence End Time"
                                                 value={newRow.absenceEndTime}
-                                                onChange={(newValue) => setNewRow({ ...newRow, absenceEndTime: newValue })}
+                                                onChange={(newValue) =>
+                                                    setNewRow({
+                                                        ...newRow,
+                                                        absenceEndTime: newValue,
+                                                    })
+                                                }
                                                 shouldDisableTime={(timeValue, clockType) => {
                                                     if (clockType === 'minutes') {
                                                         return false;
                                                     }
-                                                    if (newRow.absenceStartTime && newRow.absenceStartTime > timeValue) {
+                                                    if (
+                                                        newRow.absenceStartTime &&
+                                                        newRow.absenceStartTime > timeValue
+                                                    ) {
                                                         return true;
                                                     }
                                                     return false;
@@ -190,19 +271,45 @@ const DoctorAbsencePage = () => {
                                     <TableCell>
                                         <TextField
                                             value={newRow.optionalMessage}
-                                            onChange={e => setNewRow({ ...newRow, optionalMessage: e.target.value })}
+                                            onChange={(e) =>
+                                                setNewRow({
+                                                    ...newRow,
+                                                    optionalMessage: e.target.value,
+                                                })
+                                            }
                                         />
                                     </TableCell>
                                     <TableCell>
-                                        <Button style={{ marginLeft: '10px', marginBottom: '10px' }} variant="outlined" color="success" onClick={handleSave}>Save</Button>
-                                        <Button style={{ marginLeft: '10px', marginBottom: '10px' }} variant="outlined" color="error" onClick={handleCancel}>Cancel</Button>
+                                        <Button
+                                            style={{ marginLeft: '10px', marginBottom: '10px' }}
+                                            variant="outlined"
+                                            color="success"
+                                            onClick={handleSave}
+                                        >
+                                            Save
+                                        </Button>
+                                        <Button
+                                            style={{ marginLeft: '10px', marginBottom: '10px' }}
+                                            variant="outlined"
+                                            color="error"
+                                            onClick={handleCancel}
+                                        >
+                                            Cancel
+                                        </Button>
                                     </TableCell>
                                 </TableRow>
                             )}
                         </TableBody>
                     </Table>
                 </TableContainer>
-                <Button style={{ marginTop: '20px' }} variant="contained" color="primary" onClick={handleAdd}>Add</Button>
+                <Button
+                    style={{ marginTop: '20px' }}
+                    variant="contained"
+                    color="primary"
+                    onClick={handleAdd}
+                >
+                    Add
+                </Button>
             </Box>
         </Card>
     );
