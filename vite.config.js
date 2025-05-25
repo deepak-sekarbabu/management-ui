4c3822e88b5e13dabb52f2698430c7ad6dbb724c
Deepak Sekarbabu <deepakinmail@gmail.com>
2025-05-25 21:44:37 +0200

    fix: update proxy target from https to http in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,38 +6,38 @@ import checker from 'vite-plugin-checker';
 // ----------------------------------------------------------------------
 
 export default defineConfig({
-  plugins: [
-    react(),
-    checker({
-      eslint: {
-        lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
-      },
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: /^~(.+)/,
-        replacement: path.join(process.cwd(), 'node_modules/$1'),
-      },
-      {
-        find: /^src(.+)/,
-        replacement: path.join(process.cwd(), 'src/$1'),
-      },
+    plugins: [
+        react(),
+        checker({
+            eslint: {
+                lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
+            },
+        }),
     ],
-  },
-  server: {
-    port: 3030,
-     proxy: {
-          '/api': {
-            target: 'https://localhost:8443',
-            changeOrigin: true,
-            secure: false,
-            rewrite: (path) => path.replace(/^\/api/, '')
-          }
-        }
-  },
-  preview: {
-    port: 3030,
-  },
+    resolve: {
+        alias: [
+            {
+                find: /^~(.+)/,
+                replacement: path.join(process.cwd(), 'node_modules/$1'),
+            },
+            {
+                find: /^src(.+)/,
+                replacement: path.join(process.cwd(), 'src/$1'),
+            },
+        ],
+    },
+    server: {
+        port: 3030,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080', // Changed from https to http
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+    },
+    preview: {
+        port: 3030,
+    },
 });
