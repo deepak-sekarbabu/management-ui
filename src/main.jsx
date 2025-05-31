7310fa58738f32956103821120b038fdd4617c7c
Deepak Sekarbabu <deepakinmail@gmail.com>
2025-05-31 18:27:08 +0200

    Fix React Router Upgrade

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <HelmetProvider>
-        <BrowserRouter>
+        <BrowserRouter 
+            future={{
+                v7_startTransition: true,
+                v7_relativeSplatPath: true,
+            }}
+        >
             <Suspense>
                 <App />
             </Suspense>
