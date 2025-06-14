ec7aef93604bf9f044a19ba1da5c81458edd2223
Deepak Sekarbabu <deepakinmail@gmail.com>
2025-06-14 20:16:49 +0200

    Update Vite proxy config to support multiple API routes
    This commit modifies the Vite configuration to enhance the proxy setup. It removes the direct reference to `VITE_API_BASE_URL` and uses environment variables dynamically. The proxy configuration now supports multiple routes (`/api`, `/auth`, `/clinic`, etc.) with secure connections and WebSocket support, ensuring proper routing to the backend server. Additionally, it reorganizes import statements for clarity.

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,13 +1,12 @@
-import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
 import checker from 'vite-plugin-checker';
+import { loadEnv, defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // ----------------------------------------------------------------------
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
-    const VITE_API_BASE_URL = env.VITE_API_BASE_URL;
 
     return {
         plugins: [
@@ -28,17 +27,27 @@ export default defineConfig(({ mode }) => {
                     find: /^src(.+)/,
                     replacement: path.join(process.cwd(), 'src/$1'),
                 },
+
             ],
         },
         server: {
             port: 3030,
             proxy: {
-                '/api': {
-                    target: VITE_API_BASE_URL,
+                // Proxy all API routes to the backend server
+                '^/api': {
+                    target: env.VITE_API_BASE_URL,
                     changeOrigin: true,
-                    secure: false,
-                    rewrite: (path) => path.replace(/^\/api/, ''),
+                    secure: true,
+                    ws: true,
+                    rewrite: (path) => path.replace(/^\/api/, '')
                 },
+                // Proxy other routes directly
+                '^/(auth|clinic|doctor|queue|queue-slot|doctor-absence|doctor-clinic)': {
+                    target: env.VITE_API_BASE_URL,
+                    changeOrigin: true,
+                    secure: true,
+                    ws: true
+                }
             },
         },
         preview: {
